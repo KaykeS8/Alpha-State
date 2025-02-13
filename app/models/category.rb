f09b738f6f56d8a52caaef19113d039eb68bf12d
kayke <kayke.py@gmail.com>
2025-02-13 20:51:42 -0300

    relationship with articles. has_many

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     validates :kind, presence: true, inclusion: {in: ["Philosophy", "Person development", "Neuroscience"], message: '%{value} is not valid' }
+    has_many :articles
 end
