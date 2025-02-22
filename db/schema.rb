83d49bd9e0e81fb8aeaee75dedd5a982e2d4ff6f
Kayke simao <kayke.simao@gmail.com>
2025-02-21 22:16:34 -0300

    file db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_05_230553) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_22_010848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "kind"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
