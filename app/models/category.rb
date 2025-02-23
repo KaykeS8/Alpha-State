09dc9261c65d2994ed164a2a81dc07d626d485cf
Kayke simao <kayke.py@gmail.com>
2025-02-23 11:22:07 -0300

    feat(category): add association with articles

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
     validates :kind, presence: true
     validates :kind, inclusion: {in: ["personal development", "philosophy", "neuroscience"]}
+
+    has_many :articles
 end
