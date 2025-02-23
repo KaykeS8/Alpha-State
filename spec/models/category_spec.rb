d762e7e54435b3ed1864d570066a65c613494b9e
Kayke simao <kayke.py@gmail.com>
2025-02-23 11:42:57 -0300

    test(category): context of associations created for model of category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Category, type: :model do
     it { should validate_presence_of(:kind) }
     it { is_expected.to validate_inclusion_of(:kind).in_array(["personal development", "philosophy", "neuroscience"])}
   end
+
+  context "associations" do 
+    it { should have_many(:articles)}
+  end
 end
