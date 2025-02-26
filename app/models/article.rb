733d146d605adc892dc4a53cb48e50dbcd8f9077
Kayke simao <kayke.py@gmail.com>
2025-02-25 21:31:32 -0300

    feat: rich_text for article created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_rich_text :content
 
   validates :title, presence: true
   validates :description, presence: true
