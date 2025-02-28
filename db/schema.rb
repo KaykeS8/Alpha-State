895e88b799b20fe7bb56fd2c7a95f6b6adf1f3bf
Kayke simao <kayke.py@gmail.com>
2025-02-27 22:19:46 -0300

    schema of db updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_25_235009) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_27_224246) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,8 +54,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_25_235009) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
-    t.string "cover"
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
