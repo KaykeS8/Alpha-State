991730e09a78d56a7ea3f036cc4ad936e60daf89
Kayke simao <kayke.py@gmail.com>
2025-02-27 22:10:14 -0300

    feat(article): has_one_attached add in model for attached file in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
   has_rich_text :content
+  has_one_attached :cover
 
   validates :title, presence: true
-  validates :description, presence: true
-  validates :cover, presence: true
 end
