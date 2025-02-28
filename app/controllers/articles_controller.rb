ca993589b082dc77fbc5c4ffc3878630b8aecc37
Kayke simao <kayke.py@gmail.com>
2025-02-27 22:03:30 -0300

    feat(articles): add controller actions for show, new, create, and destroy
    - Implemented `show`, `new`, `create`, and `destroy` actions for the Article resource.
    - Added `before_action` filters for user authentication and finding articles.
    - Associated articles with the currently authenticated user during creation.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,35 @@
 class ArticlesController < ApplicationController
     before_action :authenticate_user!
-    def new;end
+    before_action :find_article, only: [:show, :destroy]
+
+    def show;end
+
+    def new;
+      @article = Article.new
+    end
+
+    def create
+      @article = Article.new(articles_params)
+      @article.user = current_user
+      if @article.save 
+        redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @article.destroy
+      redirect_to root_path, status: :see_other
+    end
+
+    private
+
+    def articles_params
+      params.require(:article).permit(:title, :content, :cover, :category_id)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
+    end
 end
