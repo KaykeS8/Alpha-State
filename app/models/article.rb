d0532972f1ec0e98075df16ff36c3c697669da2b
Kayke simao <kayke.py@gmail.com>
2025-03-02 19:50:40 -0300

    feat(comment): model comment created and add validation for description

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :category
+  has_many :comments, dependent: :destroy
 
   has_rich_text :content
   has_one_attached :cover
