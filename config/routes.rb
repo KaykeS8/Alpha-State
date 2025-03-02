1d82faaea277aabdb6ca6f782bdff44cc87ac88b
Kayke simao <kayke.py@gmail.com>
2025-03-02 19:54:52 -0300

    feat(routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :articles
+  resources :articles do 
+    resources :comments, except: [:show, :index]
+  end
 end
