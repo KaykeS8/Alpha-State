2ce4ce5e95609d4e809861a9ef5828a86e92251f
Kayke simao <kayke.py@gmail.com>
2025-03-05 21:25:57 -0300

    feat(comments): add create and destroy actions with Turbo support
    - Implemented create action to allow users to add comments to articles.
    - Used Turbo Streams to update the UI dynamically upon comment creation and deletion.
    - Added destroy action to remove comments with Turbo-enabled responses.
    - Refactored set_article and set_comment methods for better code organization.
    - Ensured comments are associated with the current user upon creation.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,41 @@
 class CommentsController < ApplicationController
   before_action :set_article, only: [:create, :destroy]
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def create
     @comment = @article.comments.new(comments_params)
     @comment.user_id = current_user.id
     if @comment.save!
-      redirect_to @article
+      respond_to do |format|
+        format.html { redirect_to  @article, notice: "comment was successfully created." }
+        format.turbo_stream
+      end
     else
       render @article, status: :unprocessable_entity
     end
   end
 
+  def edit;end
+
+  def update
+  end
+
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to @article
+    respond_to do |format|
+      format.html { redirect_to @article, notice: "comment was successfully destroyed." }
+      format.turbo_stream
+    end
   end
 
   private
   def set_article
     @article = Article.find(params[:article_id])
+  end
+
+  def set_comment
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
   end
 
   def comments_params
