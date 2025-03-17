2f0d00fe537bad0fd1e5a6acceb317b132b7fcc9
Kayke simao <kayke.py@gmail.com>
2025-03-17 20:28:28 -0300

    Update database schema after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_02_190356) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_221437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,6 +78,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_02_190356) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.string "website_url"
+    t.string "x_url"
+    t.string "instagram_url"
+    t.string "linkedin_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -97,4 +109,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_02_190356) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "profiles", "users"
 end
