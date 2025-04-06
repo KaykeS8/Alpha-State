5b90b068714fadfdec53be5c97b9fb743232de44
Kayke simao <kayke.py@gmail.com>
2025-04-06 17:05:21 -0300

    Add gem kaminari

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   layout "profile"
   before_action :set_user, only: [:show, :about]
   def show
-    @last_two_articles = @user.articles.order(created_at: :desc).limit(2)
+    @last_two_articles = @user.articles.order(created_at: :desc)
   end
   
   def about;end
