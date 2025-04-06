f2610a7c113be97d0dd6cf337f475f3e2dd2949b
Kayke simao <kayke.py@gmail.com>
2025-04-06 16:10:06 -0300

    rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "validations" do 
     it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email)}
   end
 end
