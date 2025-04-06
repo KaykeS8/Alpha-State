cbcb9699f16272e44995d362e3cfa57b0411f21b
Kayke simao <kayke.py@gmail.com>
2025-04-06 16:12:22 -0300

    Fix: add Alias an route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
     resources :comments, except: [:show, :index]
   end
   resources :profiles, param: :user_name
+  get "profiles/:user_name/about", to: "profiles#about", as: "profile_about"
 end
